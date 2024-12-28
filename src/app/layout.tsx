cc5fd352d8bc2b06be78ed5be8c1029c23d76947
hjasaiwal114 <hjasaiwal114@gmail.com>
2024-12-27 16:17:02 -0800

    settup basic page with navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import TopNav from "@/components/navbar/TopNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <TopNav />
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 "use client";
-import { Button } from "@nextui-org/react";
-import { GoSmiley } from "react-icons/go";
+import { Button, Link } from "@nextui-org/react";
+import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl"> hello </h1>
-      <Button color="danger" variant="bordered" startContent={<GoSmiley />}>
-        Click me
+      <Button
+        as={Link}
+        href="/members"
+        color="primary"
+        variant="bordered"
+        startContent={<FaRegSmile />}
+      >
+        Click me!
       </Button>
     </div>
   );
